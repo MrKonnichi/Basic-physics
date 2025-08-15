c09ac2c4282bfc6ffdc8ee3d72f494ce3c5dc382
MrKonnichi <auckme@proton.me>
2025-08-15 23:35:35 +0300

    Update physics.py

--- physics.py
+++ physics.py
@@ -1,51 +1,67 @@
 import time
-print("The initial coordinates of the object are 0,0,0 (every digit represents a meter)")
-Fx= float(input("Add an initial horizontal force (x axis) (positive is right negative is left): "))
-Fy= float(input("Add an initial vertical force (y axis) ( positive is up negative is down): "))
-Fz= float(input("Add an initial depth force (z axis) (positive is forward negative is backward): "))
-m = float(input("Set mass in kg: "))
-if m<=0:
-	print("Mass must be positive")
-	exit()
-shape = input("Whats the shape (cube, sphere): ")
+#axis
+x, y, z = 0, 0, 0
+#axis forces
+Fx, Fy, Fz = 0, 0, 0
+
+m = 0.058#mass
+A=0.00353 #frontal area
+
+#sets the drag coeffecient
+shape = "sphere"
 if shape.lower() == "cube":
 	Cd=1.05
 elif shape.lower() == "sphere":
 	Cd=0.47
-else:
-	print("Please choose a cube or a sphere")
-	exit()
-A = float(input("Set the frontal area in m²: "))
-g=9.80665 
-Pair=1.225
+g=9.80665 #gravity
+Pair=1.225 #density of air
+t=1/60 #60hz
+throw=0.2 #simulating a hand throw
+#calculating acceleration
 ax=Fx/m
 ay=Fy/m
-az=Fz/m
-t=1/60
-vx=ax*0.2
-vy=ay*0.2
-vz=az*0.2
-DragX=-0.5*Pair*vx*abs(vx)*Cd*A
-DragY=-0.5*Pair*vy*abs(vy)*Cd*A
-DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
-ax=Fx/m + DragX/m
-ay=Fy/m + DragY/m -g
-az=Fz/m + DragZ/m
-x=vx*t
-y=vy*t
-z=vz*t
+az=Fz/m 
+#old velocity
+vx=0
+vy=0
+vz=0
+
+throw_steps=int(throw/t)
+for steps in range(throw_steps):
+	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
+	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
+	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
+	
+	ax = (DragX+Fx) / m
+	ay = (DragY+Fy) / m -g
+	az = (DragZ+Fz) / m
+	
+	vx+= ax * t
+	vy+= ay * t
+	vz+= az * t
+	
+	x+=vx * t
+	y+=vy * t
+	z+=vz * t
+	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
+	time.sleep(t)
 while True:
 	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
 	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
 	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
+	#Drag
 	ax= DragX/m
 	ay= -g + DragY/m
 	az= DragZ/m
-	vx=vx+ax*t
-	vy=vy+ay*t
-	vz=vz+az*t
+	#Acceleration
+	vx+=ax*t
+	vy+=ay*t
+	vz+=az*t
+	#Velocity
 	x=x+vx*t
 	y=y+vy*t
 	z=z+vz*t
+	#Displacement
 	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
+	#Printing
 	time.sleep(t)
