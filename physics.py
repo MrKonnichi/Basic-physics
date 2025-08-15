02b9d21830adf928b5226e7aa475d7c04a655890
MrKonnichi <auckme@proton.me>
2025-08-16 01:03:56 +0300

    Update physics.py

--- physics.py
+++ physics.py
@@ -1,12 +1,9 @@
 import time
-#axis
-x, y, z = 0, 0, 0
-#axis forces
-Fx, Fy, Fz = 0, 0, 0
 
+x, y, z = 0, 0, 0 #axis
+Fx, Fy, Fz = 0, 0, 0 #axis forces
 m = 0.058#mass
 A=0.00353 #frontal area
-
 #sets the drag coeffecient
 shape = "sphere"
 if shape.lower() == "cube":
@@ -21,13 +18,10 @@ throw=0.2 #simulating a hand throw
 ax=Fx/m
 ay=Fy/m
 az=Fz/m 
-#old velocity
-vx=0
-vy=0
-vz=0
-
-throw_steps=int(throw/t)
-for steps in range(throw_steps):
+vx,vy,vz=0,0,0 #old velocity
+#initial throw
+throw_hz=int(throw/t)
+for steps in range(throw_hz):
 	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
 	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
 	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
@@ -46,22 +40,22 @@ for steps in range(throw_steps):
 	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
 	time.sleep(t)
 while True:
+	#Drag
 	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
 	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
 	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
-	#Drag
+	#Acceleration
 	ax= DragX/m
 	ay= -g + DragY/m
 	az= DragZ/m
-	#Acceleration
+	#Velocity
 	vx+=ax*t
 	vy+=ay*t
 	vz+=az*t
-	#Velocity
+	#Displacement
 	x=x+vx*t
 	y=y+vy*t
 	z=z+vz*t
-	#Displacement
+	#printing coordinates
 	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
-	#Printing
 	time.sleep(t)
