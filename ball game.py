0f0dffe6fc3bae4514f91f06332d7b8f54cd20d9
MrKonnichi <auckme@proton.me>
2025-08-16 01:03:27 +0300

    Update ball game.py

--- ball game.py
+++ ball game.py
@@ -1,11 +1,9 @@
 import pygame
-#axis
-x, y, z = 0, 0, 0
-#axis forces
-Fx, Fy, Fz = 5, 40, 0
 
+x, y, z = 0, 0, 0 #axis
+Fx, Fy, Fz = 5, 40, 0 #axis forces
 m = 0.75 #mass
-
+#sets the drag coeffecient
 shape = "sphere"
 if shape == "cube":
 	Cd=1.05
@@ -13,24 +11,27 @@ elif shape == "sphere":
 	Cd=0.47
 radius=20
 A = (radius/100)**2*3.141
-g=9.80665 
-Pair=1.225
-t=1/60
-throw=0.2
+g=9.80665 #gravity
+Pair=1.225 #density of air
+t=1/60 #60hz
+throw=0.2 #simulating a hand throw
+#calculating acceleration
 ax=Fx/m
 ay=Fy/m
 az=Fz/m
-vx,vy,vz=0,0,0
+vx,vy,vz=0,0,0 #old velocity
 pygame.init()
 info = pygame.display.Info()
 screen = pygame.display.set_mode((info.			current_w, info.current_h))
 running=True
+#setting colors
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
 clock = pygame.time.Clock()
-throw_steps=int(throw/t)
-for steps in range(throw_steps):
+#initial throw
+throw_hz=int(throw/t)
+for steps in range(throw_hz):
 	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
 	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
 	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
@@ -49,22 +50,28 @@ for steps in range(throw_steps):
 	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
 	clock.tick(60)
 while True:
-	screen.fill(black)
+	screen.fill(black) #fills the screen with black to remove previous ball positions
+	pygame.draw.rect(screen, red, (0, info.current_h-100, info.current_w, 100)) #draw ground
+	#fixes the weird pygame coordinates system and makes the ball stand in the ground
 	FixX=int(x*100+info.current_w / 2)
 	FixY=int(y*-100+info.current_h / 2)
-	if FixY>info.current_h-101:
-		FixY=info.current_h-100
-	pygame.draw.circle(screen,white,(FixX,FixY),radius)
-	pygame.display.update()
+	if FixY>info.current_h-121:
+		FixY=info.current_h-120
+	pygame.draw.circle(screen,white,(FixX,FixY),radius) #draw the ball
+	pygame.display.update() #refresh screen
+	#drag
 	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
 	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
 	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
+	#acceleration
 	ax= DragX/m
 	ay= -g + DragY/m
 	az= DragZ/m
+	#velocity
 	vx=vx+ax*t
 	vy=vy+ay*t
 	vz=vz+az*t
+	#displacement
 	x=x+vx*t
 	y=y+vy*t
 	z=z+vz*t
