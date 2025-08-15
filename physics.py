4adea9041c21baac1a9bda26d204cde25b224c2b
MrKonnichi <auckme@proton.me>
2025-08-15 13:48:53 +0300

    Update physics.py

--- physics.py
+++ physics.py
@@ -3,10 +3,19 @@ print("The initial coordinates of the object are 0,0,0 (every digit represents a
 Fx= float(input("Add a horizontal force (positive is right negative is left): "))
 Fy= float(input("Add a vertical force ( positive is up negative is down): "))
 Fz= float(input("Add a depth force (positve is forward negative is backward): "))
-m = float(input("Set mass: "))
+m = float(input("Set mass in kg: "))
 if m<=0:
 	print("Mass must be positive")
 	exit()
+shape = input("Whats the shape (cube, sphere): ")
+if shape.lower() == "cube":
+	Cd=1.05
+elif shape.lower() == "sphere":
+	Cd=0.47
+else:
+	print("Please choose a cube or a sphere")
+	exit()
+A = float(input("Set the frontal area in m²: "))
 ax=Fx/m
 ay=Fy/m
 az=Fz/m
@@ -17,8 +26,6 @@ vz=az*t
 x=0
 y=0
 z=0
-Cd=0.47
-A=1
 while True:
 	DragX=-0.6125*vx*abs(vx)*Cd*A
 	DragY=-0.6125*vy*abs(vy)*Cd*A
