d9da45683e425aebde4445d5f3baed92d104cb8d
MrKonnichi <auckme@proton.me>
2025-08-17 04:38:30 +0300

    Update physics.py

--- physics.py
+++ physics.py
@@ -1,61 +1,39 @@
 import time
-
-x, y, z = 0, 0, 0 #axis
-Fx, Fy, Fz = 0, 0, 0 #axis forces
-m = 0.058#mass
-A=0.00353 #frontal area
-#sets the drag coeffecient
-shape = "sphere"
-if shape.lower() == "cube":
-	Cd=1.05
-elif shape.lower() == "sphere":
-	Cd=0.47
+import pygame
 g=9.80665 #gravity
 Pair=1.225 #density of air
-t=1/60 #60hz
-throw=0.2 #simulating a hand throw
-#calculating acceleration
-ax=Fx/m
-ay=Fy/m
-az=Fz/m 
-vx,vy,vz=0,0,0 #old velocity
-#initial throw
-throw_hz=int(throw/t)
-for steps in range(throw_hz):
-	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
-	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
-	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
-	
-	ax = (DragX+Fx) / m
-	ay = (DragY+Fy) / m -g
-	az = (DragZ+Fz) / m
-	
-	vx+= ax * t
-	vy+= ay * t
-	vz+= az * t
-	
-	x+=vx * t
-	y+=vy * t
-	z+=vz * t
-	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
-	time.sleep(t)
-while True:
-	#Drag
-	DragX=-0.5*Pair*vx*abs(vx)*Cd*A
-	DragY=-0.5*Pair*vy*abs(vy)*Cd*A
-	DragZ=-0.5*Pair*vz*abs(vz)*Cd*A
-	#Acceleration
-	ax= DragX/m
-	ay= -g + DragY/m
-	az= DragZ/m
-	#Velocity
-	vx+=ax*t
-	vy+=ay*t
-	vz+=az*t
-	#Displacement
-	x=x+vx*t
-	y=y+vy*t
-	z=z+vz*t
-	#printing coordinates
-	print(f"Coordinates: x={x:.2f}, y={y:.2f}, z={z:.2f}",end="\r")
-	time.sleep(t)
+def set_fps(fps):
+	global t
+	t=1/fps
+class Object:
+	def __init__(self,x,y,m,shape,FA):
+		self.Fx=0
+		self.Fy=0
+		self.x=x
+		self.y=y
+		self.m=m
+		self.FA=FA
+		self.vx=0
+		self.vy=0
+		self.meterx=0
+		self.metery=0
+		if shape.lower() == "cube":
+			self.Cd=1.05
+		elif shape.lower() == "sphere":
+			self.Cd=0.47
+			
+	def enable(self):
+		#Drag
+		DragX=-0.5*Pair*self.vx*abs(self.vx)*self.Cd*self.FA
+		DragY=-0.5*Pair*self.vy*abs(self.vy)*self.Cd*self.FA
+		#Acceleration
+		ax= (DragX+self.Fx)/self.m
+		ay= (DragY+self.Fy)/self.m -g
+		#Velocity
+		self.vx+=ax*t
+		self.vy+=ay*t
+		#Displacement
+		self.meterx+=self.vx*t
+		self.metery+=self.vy*t
+		self.x=int((self.meterx)*64+(pygame.display.get_surface().get_width())/2)
+		self.y=int((self.metery)*-64+(pygame.display.get_surface().get_height())/2)
